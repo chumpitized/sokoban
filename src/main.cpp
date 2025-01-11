189c7b41a69b4ddc57789f022cfe41e286771d47
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-01-11 11:31:22 -0600

    add texture scaling

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,9 @@ int main() {
 	//must always respect sprite dimensions
 	int screenWidth		= 1920;
 	int screenHeight 	= 1088;
-	int tileSize 		= 64;
+
+	//tileSize should scale with screenHeight
+	int tileSize 		= 192;
 
 	SetTargetFPS(60);
 	InitWindow(screenWidth, screenHeight, "Sokoban!");
@@ -36,28 +38,25 @@ int main() {
 		3,3,3,3,3
 	};
 
-	Texture2D texture = load_sprites("data/sprites/wall.png");
+	Texture2D texture = load_sprites("data/sprites/wall_horizontal.png");
 
 	while(!WindowShouldClose()) {
 
 		//Draw game screen to Texture2D
 		BeginTextureMode(game);
-			for (int i = 0; i < first.size(); ++i) {
+			for (int i = 0; i < first.size(); ++i) { 
 				int xOffset = (i % 5);
 				int yOffset = (i) / 5;
 
 				int windowXOffset = getOffset(tileSize, 5, screenWidth);
 				int windowYOffset = getOffset(tileSize, 5, screenHeight);
 
-				std::cout << "X OFFSET: " << xOffset << " || Y OFFSET: " << yOffset << std::endl;
-
 				//Texture2D texture = load_sprites("data/sprites/wall.png");
 
-				DrawTexture(texture, (xOffset * tileSize) + windowXOffset, (yOffset * tileSize) + windowYOffset, RAYWHITE);
+				//Draw texture with extended parameters (scale, rotation, etc.)
+				DrawTextureEx(texture, Vector2{(float)(xOffset * tileSize) + windowXOffset, (float)(yOffset * tileSize) + windowYOffset}, 0.0, 12.0, RAYWHITE); 
 			}
 		EndTextureMode();
-
-
 
 
 		//Draw to window
